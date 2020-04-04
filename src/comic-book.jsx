d86e3b1ffb94d4c6b3af15435cdf0f00699e9d8f
Elaine Wang <elainewang.cal@gmail.com>
2020-04-03 20:26:54 -0700

    Initial React setup

---
+++ src/comic-book.jsx
@@ -0,0 +1,11 @@
+class ComicBook extends React.Component {
+
+	render() {
+		return (
+			<div>humbuga</div>
+		)
+	}
+}
+
+let domContainer = document.querySelector("#page-container");
+ReactDOM.render(<ComicBook />, domContainer);
