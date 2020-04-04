94defb0b8b3960f91e191b4a6776feb41f7bea82
Elaine Wang <elainewang.cal@gmail.com>
2020-04-04 00:44:38 -0700

    Add some style

--- comic-book.js
+++ comic-book.js
@@ -6,28 +6,78 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var ComicBook = function (_React$Component) {
-	_inherits(ComicBook, _React$Component);
+var ComicBookPageContainer = function (_React$Component) {
+	_inherits(ComicBookPageContainer, _React$Component);
 
-	function ComicBook() {
-		_classCallCheck(this, ComicBook);
+	function ComicBookPageContainer() {
+		_classCallCheck(this, ComicBookPageContainer);
 
-		return _possibleConstructorReturn(this, (ComicBook.__proto__ || Object.getPrototypeOf(ComicBook)).apply(this, arguments));
+		return _possibleConstructorReturn(this, (ComicBookPageContainer.__proto__ || Object.getPrototypeOf(ComicBookPageContainer)).apply(this, arguments));
 	}
 
-	_createClass(ComicBook, [{
+	_createClass(ComicBookPageContainer, [{
 		key: "render",
 		value: function render() {
 			return React.createElement(
 				"div",
-				null,
-				"humbuga"
+				{ className: "page-container" },
+				React.createElement(BookContainer, null)
 			);
 		}
 	}]);
 
-	return ComicBook;
+	return ComicBookPageContainer;
+}(React.Component);
+
+var BookContainer = function (_React$Component2) {
+	_inherits(BookContainer, _React$Component2);
+
+	function BookContainer() {
+		_classCallCheck(this, BookContainer);
+
+		return _possibleConstructorReturn(this, (BookContainer.__proto__ || Object.getPrototypeOf(BookContainer)).apply(this, arguments));
+	}
+
+	_createClass(BookContainer, [{
+		key: "render",
+		value: function render() {
+			return React.createElement(
+				"div",
+				{ className: "book-container" },
+				React.createElement(BookCover, null)
+			);
+		}
+	}]);
+
+	return BookContainer;
+}(React.Component);
+
+var BookCover = function (_React$Component3) {
+	_inherits(BookCover, _React$Component3);
+
+	function BookCover() {
+		_classCallCheck(this, BookCover);
+
+		return _possibleConstructorReturn(this, (BookCover.__proto__ || Object.getPrototypeOf(BookCover)).apply(this, arguments));
+	}
+
+	_createClass(BookCover, [{
+		key: "render",
+		value: function render() {
+			return React.createElement(
+				"div",
+				{ className: "cover page-size right-page" },
+				React.createElement(
+					"div",
+					{ className: "cover-title" },
+					"Elaine's Book of Original Comics and Other Creations"
+				)
+			);
+		}
+	}]);
+
+	return BookCover;
 }(React.Component);
 
 var domContainer = document.querySelector("#page-container");
-ReactDOM.render(React.createElement(ComicBook, null), domContainer);
+ReactDOM.render(React.createElement(ComicBookPageContainer, null), domContainer);
--- src/comic-book.jsx
+++ src/comic-book.jsx
@@ -1,11 +1,36 @@
-class ComicBook extends React.Component {
+class ComicBookPageContainer extends React.Component {
 
 	render() {
 		return (
-			<div>humbuga</div>
+			<div className="page-container">
+				<BookContainer />
+
+			</div>
+		)
+	}
+}
+
+class BookContainer extends React.Component {
+
+	render() {
+		return (
+			<div className="book-container">
+				<BookCover />
+			</div>
+		)
+	}
+}
+
+class BookCover extends React.Component {
+
+	render() {
+		return (
+			<div className="cover page-size right-page">
+				<div className="cover-title">Elaine's Book of Original Comics and Other Creations</div>
+			</div>
 		)
 	}
 }
 
 let domContainer = document.querySelector("#page-container");
-ReactDOM.render(<ComicBook />, domContainer);
+ReactDOM.render(<ComicBookPageContainer />, domContainer);
